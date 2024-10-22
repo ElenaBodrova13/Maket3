d83f9e6bbba3d3c1beda4d40e424171b95399d80
Elena Bodrova <pyx56@yandex.ru>
2024-10-22 19:51:41 +0300

    Первый коммит

---
+++ main.js
@@ -0,0 +1,42 @@
+
+  
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    
+    spaceBetween:0,
+    loop: true,
+   
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    slidesPerView: 1.2,
+  });
+  
+
+  let buttonElse = document.querySelector(".collection__else");
+  let submenu = document.querySelector(".submenu");
+  let row = document.querySelector(".collection__rows");
+  console.log(row);
+  buttonElse.addEventListener("click", function(){
+    submenu.classList.toggle("collection__submenu-hidden");
+    
+    if (submenu.classList.contains("collection__submenu-hidden")) {
+        buttonElse.textContent="Показать все";
+        row.classList.remove("rowsupp");
+        row.classList.add("rowsdown");
+     
+
+        buttonElse.appendChild(row);
+    } else {
+        buttonElse.textContent="Скрыть";
+      
+       row.classList.remove("rowsdown");
+       row.classList.add("rowsupp");
+         buttonElse.appendChild(row);
+        
+    }
+    
+  });
+  
+ 
